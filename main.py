3b7d63c978909aaf70aa24343548aa006d6eb446
Priyanka Agrawal <agrawalpriyanka1612@gmail.com>
2025-02-23 16:20:29 +0530

    replaced hardcoded reservation date with dynamic date

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+import datetime
+
 import requests
 
 url = 'https://www.tablecheck.com/en/shops/pizza-4ps-in-indiranagar/available/timetable'
+date_today = str(datetime.date.today())
 params = {
 
     "reservation[num_people_adult]": 2,
-    "reservation[start_date]": '2025-02-23'
+    "reservation[start_date]": date_today
 
 }
 response = requests.request("GET", url,params=params)
 
 timetable = response.json()
+print(timetable)
